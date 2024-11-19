fcf74d396959a38d07ed08b23bf28a26cb5a53f9
AhmedYasserww <ahmedyasser2105ww@gmail.com>
2024-11-19 19:29:19 +0200

    Finish app bar and custom search widget

--- lib/features/presentation/views/widgets/home_view_body.dart
+++ lib/features/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gdg_task_ui/features/presentation/views/widgets/custom_search_widget.dart';
+import 'package:gdg_task_ui/features/presentation/views/widgets/home_view_app_bar.dart';
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
@@ -7,7 +9,8 @@ class HomeViewBody extends StatelessWidget {
     return const SingleChildScrollView(
       child: Column(
         children: [
-
+HomeViewAppBar(),
+          CustomSearchWidget(),
         ],
       ),
     );
